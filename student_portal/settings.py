c43999c43347b43ed06b6ef8708f0a7757d3f375
Vinay Kumar Yadav <vinaykryadav8081@gmail.com>
2026-06-02 12:15:43 +0530

    deployment setup

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
